59a7e034366c708f2cc22eea2a08ccae40d9a8af
struthj <struthj@oregonstate.edu>
2017-11-12 14:47:29 -0800

    travis test

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -13,7 +13,7 @@ HOST_DEPENDENCIES="debootstrap qemu-user-static binfmt-support sbuild"
 GUEST_DEPENDENCIES="build-essential git m4 sudo python"
 
 # Command used to run the tests
-TEST_COMMAND="make test"
+TEST_COMMAND="echo test"
 
 function setup_arm_chroot {
     # Host dependencies
