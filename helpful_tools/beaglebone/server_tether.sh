db391b3b02c739eb21fcd9d39e16c346d22d1134
zainkai <turkingtonkevin@gmail.com>
2017-12-16 20:49:55 -0800

    working server tether script

--- helpful_tools/beaglebone/server_tether.sh
+++ helpful_tools/beaglebone/server_tether.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # run this on the computer first
-ifconfig enp3s0 192.168.7.1
-iptables --table nat --append POSTROUTING --out-interface wlan0 -j MASQUERADE
-iptables --append FORWARD --in-interface eth1 -j ACCEPT
+ifconfig enx38d269442378 192.168.7.1
+iptables --table nat --append POSTROUTING --out-interface enp3s0 -j MASQUERADE
+iptables --append FORWARD --in-interface enx38d269442378 -j ACCEPT
 echo 1 > /proc/sys/net/ipv4/ip_forward
