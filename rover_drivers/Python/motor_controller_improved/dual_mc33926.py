55d02a8286ab6906cca07369dff922ccac4a0436
zainkai <turkingtonkevin@gmail.com>
2018-02-01 00:07:05 -0800

    adding docstrings

--- rover_drivers/Python/motor_controller_improved/dual_mc33926.py
+++ rover_drivers/Python/motor_controller_improved/dual_mc33926.py
@@ -5,11 +5,18 @@ import sys
 
 #move to a constants file.
 MAX_SPEED = 480  # 19.2 MHz / 2 / 480 = 20 kHz
-io_initialized = False
+IO_INITIALIZED = False
 
 def io_init():
-    global io_initialized
-    if io_initialized:
+    """
+    Checks and initializes all GPIO pins for motor drivers.
+
+    Note:
+        This must be initialized before constructing a MotorDriver object.
+    """
+
+    global IO_INITIALIZED
+    if IO_INITIALIZED:
         return
 
     wiringpi.wiringPiSetupGpio()
@@ -25,23 +32,22 @@ def io_init():
     wiringpi.pinMode(24, wiringpi.GPIO.OUTPUT)
     wiringpi.pinMode(25, wiringpi.GPIO.OUTPUT)
 
-    io_initialized = True
+    IO_INITIALIZED = True
+
 
 class Motor(object):
+    """
+    Enables/Disables and sets speed and direction of single motor.
+    """
     def __init__(self, pwm_pin, direction_pin, enable_pin):
         self.pwm_pin = pwm_pin
         self.direction_pin = direction_pin
         self.enable_pin = enable_pin
-
     def enable(self):
-        io_init()
         wiringpi.digitalWrite(self.enable_pin, 1)
-
     def disable(self):
-        io_init()
         wiringpi.digitalWrite(self.enable_pin, 0)
-
-    def setSpeed(self, new_speed, motorDirection):
+    def set_speed(self, new_speed, motorDirection):
         if new_speed > MAX_SPEED:
             new_speed = MAX_SPEED
         elif new_speed < 0:
@@ -51,10 +57,10 @@ class Motor(object):
         wiringpi.pwmWrite(self.pwm_pin, new_speed)
 
 
-
-
-#note: for direction values use Direction class.
-class motorDrive(object):
+class MotorDriver(object):
+    """
+    Allows users to enable/disable and control both motors speeds as well as directions.
+    """
     def __init__(self):
         self.motor1 = Motor(12, 24, 22)
         self.motor2 = Motor(13, 25, 23)
@@ -64,17 +70,16 @@ class motorDrive(object):
     def disable(self):
         self.motor1.disable()
         self.motor2.disable()
-    def setSpeeds(self, m1_speed, m1_dir, m2_speed, m2_dir):
-        self.motor1.setSpeed(m1_speed,m1_dir)
-        self.motor2.setSpeed(m2_speed,m2_dir)
-
+    def set_speeds(self, m1_speed, m1_dir, m2_speed, m2_dir):
+        self.motor1.set_speed(m1_speed, m1_dir)
+        self.motor2.set_speed(m2_speed, m2_dir)
 
 if __name__ == "__main__":
-    mDrive = motorDrive()
+    M_DRIVER = MotorDriver()
     io_init()
 
     if "-h" in sys.argv or "-help" in sys.argv:
-        print("Help text for pololu dual_mc33926.")
+        print("Help text for Pololu dual_mc33926.")
         print("-h, -help : for help text.")
         print("-e, -enable : to enable both motors.")
         print("-d, -disable : to disable both motors.")
@@ -83,17 +88,17 @@ if __name__ == "__main__":
         print("\tpwm range: 0 - 480\n\tdirections: 0 - forward, 1 - backward")
     elif "-e" in sys.argv or "-enable" in sys.argv:
         print("Enabling Motors...")
-        mDrive.enable()
+        M_DRIVER.enable()
     elif "-d" in sys.argv or "-disable" in sys.argv:
         print("Disabling Motors...")
-        mDrive.disable()
+        M_DRIVER.disable()
     elif ("-ss" in sys.argv or "-setspeed" in sys.argv) and len(sys.argv) == 6:
-        trunArg = sys.argv[2:]
-        print("setting motor1 to pwm: {0}").format(int(trunArg[0]))
-        print("setting motor1 to direction: {0}").format(int(trunArg[1]))
-        print("setting motor2 to pwm: {0}").format(int(trunArg[2]))
-        print("setting motor2 to direction: {0}").format(int(trunArg[3]))
-        mDrive.setSpeeds( (int(trunArg[0])) , (int(trunArg[1])) , (int(trunArg[2])) , (int(trunArg[3])) )
+        ARGS = sys.argv[2:]
+        print("setting motor1 to pwm: {0}").format(int(ARGS[0]))
+        print("setting motor1 to direction: {0}").format(int(ARGS[1]))
+        print("setting motor2 to pwm: {0}").format(int(ARGS[2]))
+        print("setting motor2 to direction: {0}").format(int(ARGS[3]))
+        M_DRIVER.set_speeds(int(ARGS[0]), int(ARGS[1]), int(ARGS[2]), int(ARGS[3]))
     else:
         print("Invalid command arguments.")
         print("-h, -help : for help text.")
