b87507d14aac8ef3a70e14a9b5f69f22cf3608db
Kevin Turkington <turkingtonkevin@gmail.com>
2018-02-02 15:05:13 -0800

    replacing timeout with a constant

--- rover_drivers/Python/sonar_driver/mb7360-hrxl.py
+++ rover_drivers/Python/sonar_driver/mb7360-hrxl.py
@@ -18,10 +18,9 @@ def io_init_sonar(rd_pin,trig_pin):
     wiringpi.pinMode(rd_pin, wiringpi.GPIO.OUTPUT)
 
 class sonar_control(object):
-    def __init__(self, rd_pin,trig_pin,timeout):
+    def __init__(self, rd_pin,trig_pin):
         self.rd_pin = rd_pin
         self.trig_pin = trig_pin
-        self.timeout = timeout
     def get_range():
         # enable trigger.
         wiringpi.digitalWrite(self.trig_pin, 1)
@@ -32,10 +31,10 @@ class sonar_control(object):
         time_delta = time.time() - start_time
 
         # only record time until read pin is high or time delta exceeds timeout.
-        while wiringpi.digitalRead(self.rd_pin) != 1 or time_delta < self.timeout:
+        while wiringpi.digitalRead(self.rd_pin) != 1 or time_delta < SONAR_TIMEOUT:
             time_delta = time.time() - start_time
 
-        if time_delta > self.timeout:
+        if time_delta > SONAR_TIMEOUT:
             return -1
 
         return time_delta / SONAR_CALIBRATION #smallest unit of distance
