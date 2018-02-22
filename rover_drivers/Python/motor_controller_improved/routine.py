c93583e4514e938b6c14801c8cb92fa3db629d84
zainkai <turkingtonkevin@gmail.com>
2018-02-21 23:23:20 -0800

    movement script for testing.

---
+++ rover_drivers/Python/motor_controller_improved/routine.py
@@ -0,0 +1,34 @@
+import dual_mc33926, time
+# (testing) add this script for startup
+# sudo nano .bashrc
+
+def forward():
+    M_DRIVER = dual_mc33926.MotorDriver()
+    M_DRIVER.set_speeds(240,1,240,1)
+    M_DRIVER.enable()
+    time.sleep(30)
+    M_DRIVER.disable()
+
+def box():
+    M_DRIVER = dual_mc33926.MotorDriver()
+    M_DRIVER.set_speeds(240,1,240,1)
+    M_DRIVER.enable()
+    time.sleep(5)
+
+    for i in range(1,3):
+        M_DRIVER.set_speeds(240,0,240,1)
+        time.sleep(2)
+
+        M_DRIVER.set_speeds(240,1,240,1)
+        time.sleep(5)
+    
+    M_DRIVER.disable()
+
+if __name__ == "__name__":
+    dual_mc33926.io_init()
+    forward()
+
+
+
+
+
