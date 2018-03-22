d390639822eae56972ea0b1939d799675f685ae9
Kevin Turkington <turkingtonkevin@gmail.com>
2018-03-22 15:37:31 -0700

    servooooooooooooooooooooooooooooooooo

---
+++ rover_drivers/Python/solar_servo/servo.py
@@ -0,0 +1,19 @@
+import time
+import RPi.GPIO as GPIO
+
+
+def deploy_servos():
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(12,GPIO.OUT)
+
+    pwm = GPIO.PWM(12,50)
+    pwm.start(0)
+
+    for i in range(0,50):
+        pwm.ChangeDutyCycle(5)
+        time.sleep(0.1)
+        pwm.ChangeDutyCycle(0)
+        time.sleep(0.1)
+
+    pwm.stop()
+    GPIO.cleanup()
